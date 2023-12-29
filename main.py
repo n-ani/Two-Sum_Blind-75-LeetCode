1d8bd252e5c63ae34500949aab03923ba682f1c0
ANIRBAN ADHIKARY <54547722+n-ani@users.noreply.github.com>
2023-12-30 02:51:13 +0530

    main.py

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].'''
 
 #Code
+
+# Collecting the target value.
 target=int(input("Enter Target Number: "))
+# Collecting the inputs in the array using map function in one line seperating with space.
 arr=list(map(int,input("Enter Data in the Array: ").strip(" ").split(" ")))
 
-#target=5
+'''You can use this example for checking the output instantly
+target=5
+arr=[1,2,3,4,5,0]'''
 
-#arr=[1,2,3,4,5,0]
+#print the header.
 print("|| Output || \n------------\nIndex 1 | value 1 + Index 2 | Value 2 = Result")
+#for loop
 for i in range(len(arr)):
+  #capture the first index as front value.
   set=arr[i]
+  #inner for loop for calculating the values between the front index and rare indexes. 
   for j in range(i+1,len(arr)):
     if set+arr[j]==target:
       print("\n   {}    |    {}    +    {}    |    {}    =   {}".format(arr.index(arr[i]),arr[i],arr.index(arr[j]),arr[j],(arr[i]+arr[j])))
